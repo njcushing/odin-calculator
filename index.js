f95d14acdd79c30a592e5cd66217f20d47f6ad13
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-20 09:15:37 +0100

    Create operate function to select appropriate calculation

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+let numOne = 0;
+let operator = '';
+let numTwo = 0;
+
+function operate(operator) {
+    let result = 0;
+    switch(operator){
+        case '+': result = add(numOne, numTwo); break;
+        case '-': result = subtract(numOne, numTwo); break;
+        case '*': result = multiply(numOne, numTwo); break;
+        case '/': result = divide(numOne, numTwo); break;
+    }
+    numOne = result;
+    return result;
+}
+
 function add(numOne, numTwo) {
     return numOne + numTwo;
 }
