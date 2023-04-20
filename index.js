701ef0358dd4caf5bbcc642c1c57158eb6ce027a
njcushing <129661299+njcushing@users.noreply.github.com>
2023-04-20 09:08:50 +0100

    Create add, subtract, multiply and divide functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function add(numOne, numTwo) {
+    return numOne + numTwo;
+}
+function subtract(numOne, numTwo) {
+    return numOne - numTwo;
+}
+function multiply(numOne, numTwo) {
+    return numOne * numTwo;
+}
+function divide(numOne, numTwo) {
+    return numOne / numTwo;
+}
